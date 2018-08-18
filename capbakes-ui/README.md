ad761c79bf06adc52e174502bf0deab2837104f2
gouthammavuri <gmavuri@gmail.com>
2018-08-18 17:36:59 +0530

    capbakes-ui added

--- capbakes-ui/README.md
+++ capbakes-ui/README.md
@@ -1,4 +1,4 @@
-# Capbakes
+# CapbakesUi
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.1.4.
 
--- capbakes-ui/src/app/app.component.spec.ts
+++ capbakes-ui/src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'capbakes'`, async(() => {
+  it(`should have as title 'capbakes-ui'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('capbakes');
+    expect(app.title).toEqual('capbakes-ui');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to capbakes!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to capbakes-ui!');
   }));
 });
